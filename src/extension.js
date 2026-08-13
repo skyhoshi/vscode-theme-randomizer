9818f0d0832c4b4f830464c681b1e281db0ad08b
Marcus Kern <skyacem@hotmail.com>
2026-08-13 19:57:14 +0000

    fix: address follow-up review comments
    Co-authored-by: skyhoshi <5558629+skyhoshi@users.noreply.github.com>

--- src/extension.js
+++ src/extension.js
@@ -1,25 +1,12 @@
 const { createExtensionApi } = require('./themeRandomizer');
 
-let defaultApi;
-
-function getDefaultApi() {
-  if (!defaultApi) {
-    // Loaded lazily so tests can import this file without VS Code runtime.
-    // eslint-disable-next-line global-require
-    const vscode = require('vscode');
-    defaultApi = createExtensionApi(vscode);
-  }
-
-  return defaultApi;
-}
-
 function activate(context) {
-  return getDefaultApi().activate(context);
+  // eslint-disable-next-line global-require
+  const vscode = require('vscode');
+  return createExtensionApi(vscode).activate(context);
 }
 
-function deactivate() {
-  return getDefaultApi().deactivate();
-}
+function deactivate() {}
 
 module.exports = {
   activate,
